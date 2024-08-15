bea826011b65dcd70c649307505bfb723b6b483a
nguyenducviet22 <ndv6060@gmail.com>
2024-08-15 22:21:01 +0700

    update 15/8

--- EmployeeManagementSystem/EmployeeManagementSystem/src/employeemanagementsystem/ConsoleInputService.java
+++ EmployeeManagementSystem/EmployeeManagementSystem/src/employeemanagementsystem/ConsoleInputService.java
@@ -5,6 +5,9 @@
  */
 package employeemanagementsystem;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,9 +19,14 @@ import java.util.regex.Pattern;
 public class ConsoleInputService {
     private static ConsoleInputService instance;
     private Scanner sc;
-    private final String ALPHABET_AND_NUMBER_FORMAT = "[a-zA-Z0-9]+";
+    private final String ONLY_ALPHABET_AND_NUMBER_FORMAT = "[a-zA-Z0-9]+";
+    private final String ONLY_NUMBER_FORMAT = "[0-9]+";
+    private final String EMPLOYEE_NAME_FORMAT = "[A-Z][a-z]+(\\s[A-Z][a-z]+)*";
+    private final String PHONE_FORMAT = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$";
+    private final String EMAIL_FORMAT = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    private final String ADDRESS_FORMAT = "^[#.0-9a-zA-Z\\s,-]+$";
 
-    public ConsoleInputService() {
+    private ConsoleInputService() {
         sc = new Scanner(System.in);
     }
     
@@ -33,10 +41,63 @@ public class ConsoleInputService {
         return sc.nextLine();
     }
     
-    public boolean checkStringContainOnlyAlphabetAndNumber(String value){
-        Pattern pattern = Pattern.compile(ALPHABET_AND_NUMBER_FORMAT);
+    public boolean isContainOnlyAlphabetAndNumber(String value){
+        return checkStringWithPattern(ONLY_ALPHABET_AND_NUMBER_FORMAT, value);
+    }
+    
+    public boolean isContainOnlyNumber(String value){
+        return checkStringWithPattern(ONLY_NUMBER_FORMAT, value);
+    }
+    
+    public boolean isValidEmployeeIdFormat(String value){
+        return checkStringWithPattern(ONLY_ALPHABET_AND_NUMBER_FORMAT, value);
+    }
+    
+    public boolean isValidEmployeeNameFormat(String value){
+        return checkStringWithPattern(EMPLOYEE_NAME_FORMAT, value);
+    }
+    
+    public boolean isValidPhoneNumberFormat(String value){
+        return checkStringWithPattern(PHONE_FORMAT, value);
+    }
+    
+    public boolean isValidEmailFormat(String value){
+        return checkStringWithPattern(EMAIL_FORMAT, value);
+    }
+    
+    public boolean isValidAddressFormat(String value){
+        return checkStringWithPattern(ADDRESS_FORMAT, value);
+    }
+    
+    public boolean isValidAgencyFormat(String value){
+        return checkStringWithPattern(ONLY_ALPHABET_AND_NUMBER_FORMAT, value);
+    }
+    
+    private boolean checkStringWithPattern(String regexPattern, String value){
+        Pattern pattern = Pattern.compile(regexPattern);
         Matcher matcher = pattern.matcher(value);
         return matcher.matches();
+    }
+    
+    public boolean isValidDate(String value, String dateFormat){
+        Date date = null;
+        DateFormat sdf = new SimpleDateFormat(dateFormat);
+        sdf.setLenient(false);
+        try {
+            date = sdf.parse(value);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean isValidSalary(String value){
+        try {
+            double salary = Double.parseDouble(value);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
     
 }
--- EmployeeManagementSystem/EmployeeManagementSystem/src/employeemanagementsystem/EmployeeService.java
+++ EmployeeManagementSystem/EmployeeManagementSystem/src/employeemanagementsystem/EmployeeService.java
@@ -15,11 +15,12 @@ import java.util.List;
  */
 public class EmployeeService {
     private static EmployeeService instance;
-    
     private List<Employee> listEmployee;
+    private ConsoleInputService consoleInputService;
 
     private EmployeeService() {
         listEmployee = new ArrayList<>();
+        consoleInputService = ConsoleInputService.getInstance();
     }
     
     private static EmployeeService getInstance(){
@@ -34,6 +35,129 @@ public class EmployeeService {
 //3. Remove employees
 //4. Search employees
 //5. Sort employees by salary
+    
+    private Employee createNewEmployee(){
+//        Employee newEm = new Employee(inputEmployeeId(), inputEmployeeFirstName(), inputEmployeeLastName(), inputPhoneNumber(), 
+//                                    inputEmail(), inputAddress(), inputDOB(), inputSex(), inputSalary(), inputAgency());
+        Employee newEm = new Employee();
+        newEm.setId(inputEmployeeId());
+        newEm.setFirstName(inputEmployeeFirstName());
+        newEm.setLastName(inputEmployeeLastName());
+        newEm.setPhone(inputPhoneNumber());
+        newEm.setEmail(inputEmail());
+        newEm.setAddress(inputAddress());
+        newEm.setDob(inputDOB());
+        newEm.setSex(inputSex());
+        newEm.setSalary(inputSalary());
+        newEm.setAgency(inputAgency());
+        return newEm;
+    }
+    
+    private String inputEmployeeId(){
+        System.out.print("ID: ");
+        String idInput = consoleInputService.getStringFromConsole();//let users input value
+        if (consoleInputService.isValidEmployeeIdFormat(idInput)){
+            return idInput;//valid input
+        }
+        System.out.print("Inlavid ID, enter again: ");
+        return inputEmployeeId();//invalid input, re-input
+    }
+    
+    private String inputEmployeeFirstName(){
+        System.out.print("First Name: ");
+        String fNameInput = consoleInputService.getStringFromConsole();
+        if (consoleInputService.isValidEmployeeNameFormat(fNameInput)){
+            return fNameInput;
+        }
+        System.out.print("Inlavid First Name, enter again: ");
+        return inputEmployeeFirstName();
+    }
+    
+    private String inputEmployeeLastName(){
+        System.out.print("Last Name: ");
+        String lNameInput = consoleInputService.getStringFromConsole();
+        if (consoleInputService.isValidEmployeeNameFormat(lNameInput)){
+            return lNameInput;
+        }
+        System.out.print("Inlavid Last Name, enter again: ");
+        return inputEmployeeFirstName();
+    }
+    
+    private String inputPhoneNumber(){
+        System.out.print("Phone Number: ");
+        String phoneNumInput = consoleInputService.getStringFromConsole();
+        if (consoleInputService.isValidPhoneNumberFormat(phoneNumInput)){
+            return phoneNumInput;
+        }
+        System.out.print("Inlavid Phone Number, enter again: ");
+        return inputPhoneNumber();
+    }
+    
+    private String inputEmail(){
+        System.out.print("Email: ");
+        String emailInput = consoleInputService.getStringFromConsole();
+        if (consoleInputService.isValidEmailFormat(emailInput)){
+            return emailInput;
+        }
+        System.out.print("Inlavid Email, enter again: ");
+        return inputEmail();
+    }
+    
+    private String inputAddress(){
+        System.out.print("Address: ");
+        String addressInput = consoleInputService.getStringFromConsole();
+        if (consoleInputService.isValidAddressFormat(addressInput)){
+            return addressInput;
+        }
+        System.out.print("Inlavid Address, enter again: ");
+        return inputAddress();
+    }
+    
+    private String inputDOB(){
+        System.out.print("Date of birth: ");
+        String dobInput = consoleInputService.getStringFromConsole();
+        if (consoleInputService.isValidDate(Employee.DATE_OF_BIRTH, dobInput)){
+            return dobInput;
+        }
+        System.out.print("Inlavid Date of birth, enter again: ");
+        return inputAddress();
+    }
+    
+    private String inputSex(){
+        System.out.println("Sex (Gender): ");
+        System.out.println("1. Male");
+        System.out.println("2. Female");
+        System.out.println("3. Other");
+        
+        String choice = consoleInputService.getStringFromConsole();
+        switch (choice){
+            case "1": return "Male";
+            case "2": return "Female";
+            case "3": return "Other";
+            default: System.out.println("Invalid selection!");
+        }
+        return inputSex();
+    }
+    
+    private double inputSalary(){
+        System.out.print("Salary: ");
+        String salaryInput = consoleInputService.getStringFromConsole();
+        if (consoleInputService.isValidSalary(salaryInput)){
+            return Double.parseDouble(salaryInput);
+        }
+        System.out.print("Inlavid Salary, enter again: ");
+        return inputSalary();
+    }
+    
+    private String inputAgency(){
+        System.out.print("Agency: ");
+        String agencyInput = consoleInputService.getStringFromConsole();
+        if (consoleInputService.isValidAgencyFormat(agencyInput)){
+            return agencyInput;
+        }
+        System.out.print("Inlavid Agency, enter again: ");
+        return inputAgency();
+    }
     
     public void addEmployee(){
         
