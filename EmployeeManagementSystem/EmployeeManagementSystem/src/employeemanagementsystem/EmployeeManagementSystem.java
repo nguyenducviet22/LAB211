f590b090186fc9840090a60f4bac61b1bca94796
nguyenducviet22 <ndv6060@gmail.com>
2024-08-16 22:32:24 +0700

    update 16/8

--- EmployeeManagementSystem/EmployeeManagementSystem/src/employeemanagementsystem/EmployeeManagementSystem.java
+++ EmployeeManagementSystem/EmployeeManagementSystem/src/employeemanagementsystem/EmployeeManagementSystem.java
@@ -5,17 +5,30 @@
  */
 package employeemanagementsystem;
 
+import employeemanagementsystem.employee.EmployeeService;
+
 /**
  *
  * @author NGUYEN DUC VIET
  */
 public class EmployeeManagementSystem {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static EmployeeService employeeService;
+    private static ConsoleInputService consoleInputService;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        employeeService = EmployeeService.getInstance();
+        consoleInputService = ConsoleInputService.getInstance();
     }
     
+    static void printMenu(){
+        System.out.println("MAIN MENU");
+        System.out.println("0. Show all");
+        System.out.println("1. Add");
+        System.out.println("2. Update");
+        System.out.println("3. Remove");
+        System.out.println("4. Search");
+        System.out.println("5. Sort by salary");
+        System.out.println("6. Exit");
+    }
 }
